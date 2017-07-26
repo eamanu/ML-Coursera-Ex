f19cf6353d6da92bd3a3df6da37792653d1fd0d1
eamanu <emmanuelarias30@gmail.com>
2017-07-26 18:38:15 -0300

    Complete ex6 and ex6_spma part1

--- week7/machine-learning-ex6/ex6/dataset3Params.m
+++ week7/machine-learning-ex6/ex6/dataset3Params.m
@@ -23,11 +23,24 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+ar = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+a = 1;
+for i = 1:8 
+  for j = 1:8
+    C = ar(i);
+    sigma = ar(j);
+    model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));     
+    predictions = svmPredict(model, Xval);
+    c(a) = C;
+    s(a) = sigma;
+    er(a) = mean(double(predictions ~= yval));
+    a = a+1;
+  end
+end
 
-
-
-
-
+[e, ie] = min(er);
+C = c(ie);
+sigma = s(ie);
 
 % =========================================================================
 
